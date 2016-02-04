b60883742bad7dc55cac508f08ea2f3a30a21bb2
Kenton Hirowatari <kenton@hirowatari.ca>
2016-02-04 07:34:40 -0800

    Create/drop data db before/after tests

--- plugin/tests/_support/WordPressHelper.php
+++ plugin/tests/_support/WordPressHelper.php
@@ -10,10 +10,18 @@ namespace Codeception\Module;
 class WordPressHelper extends \Codeception\Module
 {
     private $path_to_wordpress = '/var/www/html/';
+    private $database_name = 'wordpress';
 
     public function _beforeSuite() {
         if (!file_exists("{$this->path_to_wordpress}wp-config-sample.php")) {
             exec("wp core download --path={$this->path_to_wordpress}");
         }
+        $db = new \mysqli('db', 'root', $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']);
+        $db->prepare("CREATE DATABASE {$this->database_name};")->execute();
+    }
+
+    public function _afterSuite() {
+        $db = new \mysqli('db', 'root', $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']);
+        $db->prepare("DROP DATABASE {$this->database_name};")->execute();
     }
 }
