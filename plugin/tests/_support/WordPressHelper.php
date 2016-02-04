a6f55672bb0d3c8bc209eadebba74be14e6bf9c5
Kenton Hirowatari <kenton@hirowatari.ca>
2016-02-04 07:42:20 -0800

    Install WordPress before running testing

--- plugin/tests/_support/WordPressHelper.php
+++ plugin/tests/_support/WordPressHelper.php
@@ -11,13 +11,21 @@ class WordPressHelper extends \Codeception\Module
 {
     private $path_to_wordpress = '/var/www/html/';
     private $database_name = 'wordpress';
+    private $database_host = 'db';
+    private $database_user = 'root';
 
     public function _beforeSuite() {
         if (!file_exists("{$this->path_to_wordpress}wp-config-sample.php")) {
             exec("wp core download --path={$this->path_to_wordpress}");
         }
-        $db = new \mysqli('db', 'root', $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']);
+        $db = new \mysqli($this->database_host, 'root', $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']);
         $db->prepare("CREATE DATABASE {$this->database_name};")->execute();
+
+        exec("wp core config --path={$this->path_to_wordpress} --dbname={$this->database_name}
+                --dbuser={$this->database_user} --dbpass={$_ENV['DB_ENV_MYSQL_ROOT_PASSWORD']}
+                --dbhost={$this->database_host}");
+        exec('wp core install --url=localhost --title="just a wordpress site" --admin_user=a
+            --admin_password=a --admin_email="a@a.a" --skip-email');
     }
 
     public function _afterSuite() {
